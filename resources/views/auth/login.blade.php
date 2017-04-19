1ca12f2a569e3de35cf5727730d0136f7144a2e0
andyarroyo5 <sonia.arroyo@udem.edu>
2017-04-19 17:01:16 -0500

    Init
    Auth

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,96 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Login</div>
+                <div class="panel-body">
+                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
+                        {{ csrf_field() }}
+
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password" class="col-md-4 control-label">Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control" name="password">
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox" name="remember"> Remember Me
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    <i class="fa fa-btn fa-sign-in"></i> Login
+                                </button>
+
+                                <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Sección Carlos  -->
+<section id="form"><!--form-->
+            <div class="container" style="text-align:center;">
+                <div class="row">
+                    <div class="col-sm-4 col-sm-offset-1">
+                        <div class="login-form"><!--login form-->
+                            <h2>Login to your account</h2>
+                            <form action="#">
+                                <img role="img" style="width:130px; height:130px;" src="" alt="Carlos Martín del Campo">
+                                <h4>Carlos Martín del Campo</h4>
+                            </form>
+                        </div><!--/login form-->
+                    </div>
+                    <div class="col-sm-1">
+                        <h2 class="or">OR</h2>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="signup-form"><!--sign up form-->
+                            <h2>Create a New Account</h2>
+                            <form action="#">
+                                <input type="text" placeholder="Name"/>
+                                <input type="email" placeholder="Email Address"/>
+                                <input type="password" placeholder="Password"/>
+                                <button type="submit" class="btn btn-default">Signup</button>
+                            </form>
+                        </div><!--/sign up form-->
+                    </div>
+                </div>
+            </div>
+        </section><!--/form-->
+@endsection
